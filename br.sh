5360a31c889b32f0a135c086c6492be26b7b739e
Marko Tasic <mtasic85@gmail.com>
2026-07-04 16:05:15 +0200

    br.sh: user/assistant dummy loop with CTRL-C (clear user message) and CTRL-D (exit)

--- br.sh
+++ br.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# trap for CTRL-C (SIGINT) to clear partial input and re-prompt cleanly
+trap '
+    user_message_content=""
+    echo " [IGNORE THIS MESSAGE]"
+    echo -n "User: "
+    # Continue to next read
+' SIGINT
+
 
 while true; do
-    read -p "User: " message
-    echo "Assistant: $message"
+    echo -n "User: "
+
+    # read user input
+    if read -r user_message_content; then
+        # read succeeds, ENTER pressed
+        echo "Assistant: $user_message_content"
+    else
+        # read failed, likely CTRL-D (EOF)
+        break
+    fi
 done
